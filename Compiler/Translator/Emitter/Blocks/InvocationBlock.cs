44d057e197bc5ee0060bd56b59661c1dd745e71d
Dominik Pytlewski <dominik@todoit.pl>
2016-10-31 14:38:42 +0100

    fixes issue

--- Compiler/Translator/Emitter/Blocks/InvocationBlock.cs
+++ Compiler/Translator/Emitter/Blocks/InvocationBlock.cs
@@ -177,6 +177,11 @@ namespace Bridge.Translator
                             inlineScript = inlineScript.Substring(6);
                         }
 
+                        if (!noThis)
+                        {
+                            Emitter.ThisRefCounter++;
+                        }
+
                         if (!isStaticMethod && noThis)
                         {
                             this.WriteThis();
